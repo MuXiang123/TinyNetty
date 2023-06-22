d28fd0e6523081b24dc55aff04b3390fb200d69a
Muxiang123 <347213872@qq.com>
2023-06-22 21:00:27 +0800

    实现服务器端

---
+++ src/main/java/io/netty/example/study/server/codec/server/Server.java
@@ -0,0 +1,52 @@
+package io.netty.example.study.server.codec.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.example.study.server.codec.Handler.OrderServerProcessHandler;
+import io.netty.example.study.server.codec.OrderFrameDecoder;
+import io.netty.example.study.server.codec.OrderFrameEncoder;
+import io.netty.example.study.server.codec.OrderProtocolDecoder;
+import io.netty.example.study.server.codec.OrderProtocolEncoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+import java.util.concurrent.ExecutionException;
+
+/**
+ * @author: zhk
+ * @description: 服务器端
+ * @date: 2023/6/22 20:44
+ * @version: 1.0
+ */
+public class Server {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        serverBootstrap.channel(NioServerSocketChannel.class);
+
+        serverBootstrap.handler(new LoggingHandler(LogLevel.INFO));
+        serverBootstrap.group(new NioEventLoopGroup());
+
+        serverBootstrap.childHandler(new ChannelInitializer<NioServerSocketChannel>() {
+            @Override
+            protected void initChannel(NioServerSocketChannel ch) throws Exception {
+                ChannelPipeline pipeline = ch.pipeline();
+                pipeline.addLast(new OrderFrameDecoder());
+                pipeline.addLast(new OrderFrameEncoder());
+                pipeline.addLast(new OrderProtocolEncoder());
+                pipeline.addLast(new OrderProtocolDecoder());
+
+                pipeline.addLast(new LoggingHandler(LogLevel.INFO));
+
+                pipeline.addLast(new OrderServerProcessHandler());
+            }
+        });
+
+        ChannelFuture channelFuture = serverBootstrap.bind(8090).sync();
+
+        channelFuture.channel().closeFuture().get();
+    }
+}
