4df5efa443ee0a16cdf4ebbbeff1cad889847943
Muxiang123 <347213872@qq.com>
2023-07-05 09:56:21 +0800

    判断channel是否能够写，避免oom

--- src/main/java/io/netty/example/study/server/handler/OrderServerProcessHandler.java
+++ src/main/java/io/netty/example/study/server/handler/OrderServerProcessHandler.java
@@ -7,6 +7,7 @@ import io.netty.example.study.common.Operation;
 import io.netty.example.study.common.OperationResult;
 import io.netty.example.study.common.RequestMessage;
 import io.netty.example.study.common.ResponseMessage;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author: zhk
@@ -20,6 +21,7 @@ import io.netty.example.study.common.ResponseMessage;
  *    ReferenceCountUtil.release(msg);
  * }
  */
+@Slf4j
 public class OrderServerProcessHandler extends SimpleChannelInboundHandler<RequestMessage> {
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, RequestMessage requestMessage) throws Exception {
@@ -36,8 +38,16 @@ public class OrderServerProcessHandler extends SimpleChannelInboundHandler<Reque
          * bug：这里写出去的时responseMessage 而不是requestMessage
          * 否则会出现client阻塞在 channelFuture.channel().closeFuture().get();
          * 无法进行FLUSH以下的步骤 read message and READ COMPLETE
+         *
+         * 没有out参数了，所以需要自己写出去
+         * ctx.writeAndFlush(responseMessage);
          */
-        //没有out参数了，所以需要自己写出去
-        ctx.writeAndFlush(responseMessage);
+
+        //改进 需要判断channel是否能够写，避免OOM
+        if(ctx.channel().isActive() && ctx.channel().isWritable()){
+            ctx.writeAndFlush(responseMessage);
+        }else{
+            log.error("not writable now, message dropped");
+        }
     }
 }
